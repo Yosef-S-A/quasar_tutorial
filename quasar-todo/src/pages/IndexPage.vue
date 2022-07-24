1710e44a6bd801d8091d340acf956734d6fd8a87
Yosef-S-A <yosefsamuel22@gmail.com>
2022-07-24 13:22:41 +0300

    new layouts

--- quasar-todo/src/pages/IndexPage.vue
+++ quasar-todo/src/pages/IndexPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-page padding>
+  <q-page class="row">
     <!-- <q-card style="width: 350px">
       <q-card-section class="bg-blue text-white"> My Card </q-card-section>
       <q-card-section>
@@ -9,12 +9,50 @@
         corporis incidunt.
       </q-card-section>
     </q-card> -->
-    <q-select
+    <!-- <q-select
       v-model="selected"
       label="beings"
       :options="['Luke', 'Yosef', 'Dawit']"
     >
-    </q-select>
+    </q-select> -->
+
+    <q-card class="col-xs-9">
+      <q-toolbar>
+        <q-input
+          :model-value="search"
+          class="full-width"
+          borderless
+          dense
+          placeholder="Search"
+        >
+          <template #append>
+            <q-icon name="mdi-magnify"></q-icon>
+          </template>
+        </q-input>
+      </q-toolbar>
+      <q-list bordered>
+        <q-item>
+          <q-item-section side>
+            <q-checkbox :model-value="true"></q-checkbox>
+          </q-item-section>
+          <q-item-section> Go Shopping </q-item-section>
+          <q-item-section side>
+            <q-btn flat round icon="mdi-delete"></q-btn>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-card>
+    <div class="col-xs-3">
+      <q-toolbar class="bg-primary">
+        <q-btn
+          fab
+          color="secondary"
+          icon="mdi-plus"
+          style="margin-bottom: -42px"
+          class="q-ml-md"
+        ></q-btn>
+      </q-toolbar>
+    </div>
   </q-page>
 </template>
 
@@ -26,7 +64,7 @@ export default defineComponent({
 
   data() {
     return {
-      selected: null,
+      search: "",
     };
   },
 });
